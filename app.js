a68935c3388cff9771be0905f6c38eff8bce9271
PINAVIBE <juan.josegn18@gmail.com>
2026-04-02 02:47:44 -0500

    imagen pokemon

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ fetch("https://pokeapi.co/api/v2/pokemon")
         .then(dato => {
             let tarjeta = document.createElement("div")
             tarjeta.textContent = dato.name
-
+            let imagen = document.createElement("img")
+            imagen.src = dato.sprites.other["official-artwork"].front_default
+            tarjeta.appendChild(imagen)
             document.getElementById("contenedor").appendChild(tarjeta)
         })
     });
